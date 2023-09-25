bdc166714efff1786330c7975983ab5ec6065e4b
David Birkhölzer <david@remotecode.it>
2023-09-25 22:47:57 +0200

    tidy up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import {type AppType} from "next/app";
 import "~/styles/globals.css";
-import {ClerkProvider, SignedIn, SignedOut} from "@clerk/nextjs";
+import {ClerkProvider} from "@clerk/nextjs";
 import {GoogleApiProvider} from "~/providers";
 
 const MyApp: AppType = ({Component, pageProps}) => {
@@ -9,12 +9,7 @@ const MyApp: AppType = ({Component, pageProps}) => {
             {...pageProps}
         >
             <GoogleApiProvider>
-                <SignedIn>
-                        <Component {...pageProps} />
-                </SignedIn>
-                <SignedOut>
-                    <Component {...pageProps} />
-                </SignedOut>
+                <Component {...pageProps} />
             </GoogleApiProvider>
         </ClerkProvider>
     );
